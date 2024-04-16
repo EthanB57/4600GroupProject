6e5d3f1704546a4266b299d9ddc86ee1f3fcbc05
EthanB57 <eab55108@uga.edu>
2024-04-16 13:34:24 -0400

    added interviewee class

---
+++ GroupProject/src/Interviewee.java
@@ -0,0 +1,25 @@
+
+public class Interviewee {
+	private String fName;
+	private String lName;
+	private String email;
+	private int year; 
+	private int score;
+	private double GPA;
+	
+	public Interviewee() {
+		
+		
+	}
+
+	public Interviewee(String fName, String lName, String email, int year, int score, double gPA) {
+		this.fName = fName;
+		this.lName = lName;
+		this.email = email;
+		this.year = year;
+		this.score = score;
+		GPA = gPA;
+	}
+	
+}
+
--- GroupProject/src/newMain.java
+++ GroupProject/src/newMain.java
@@ -1,7 +1,9 @@
-
+import java.util.Scanner;
 public class newMain {
 
 	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
 		System.out.println("hello");
-				}
+				
+	}
 }
